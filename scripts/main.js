1deca735306c086f20bd176f0f8c3d6df513a986
belldy <drieliluz@gmail.com>
2019-03-28 09:42:56 +0100

    added scripts

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+setTimeout(function() { 
+	if (confirm("Hey, le site xxx est trop bien.\nVeux-tu le visiter ?")) 
+		window.location.assign("https://www.youtube.com/watch?v=dQw4w9WgXcQ."); }, 10000);
