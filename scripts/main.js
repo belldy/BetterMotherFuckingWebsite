0b49d96bde523c93ca4ea993b71f8ecce3583324
belldy <drieliluz@gmail.com>
2019-03-28 10:20:49 +0100

    updated main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
 setTimeout(function() { 
 	if (confirm("Hey, le site xxx est trop bien.\nVeux-tu le visiter ?")) 
-window.location.assign("https://www.youtube.com/watch?v=dQw4w9WgXcQ."); }, 10000);
+window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ."; }, 10000);
+
+
+
